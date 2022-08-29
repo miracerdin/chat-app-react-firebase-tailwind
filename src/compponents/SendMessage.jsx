cd26175580b395a48d2c4545cf78bad2da8946f3
mirac <miracerdin@gmail.com>
2022-08-29 17:58:36 +0300

    useeffect

--- src/compponents/SendMessage.jsx
+++ src/compponents/SendMessage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { auth, db } from "../firebase";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 
